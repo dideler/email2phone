c1a1a1ff5ad57e1ba6d832257bb508e603545551
Dennis Ideler <ideler.dennis@gmail.com>
2014-10-04 17:04:22 +0100

    Empty session for request forgery protection

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,4 +1,4 @@
 class WebhooksController < ApplicationController
-  # TODO: enable when needed
-  #skip_before_action :verify_authenticity_token
+  #skip_before_action :verify_authenticity_token # TODO: enable when needed
+  protect_from_forgery with: :null_session
 end
