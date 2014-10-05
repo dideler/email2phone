526571e59cae123480acab8547c7a0df07782153
Dennis Ideler <ideler.dennis@gmail.com>
2014-10-05 03:05:59 +0100

    Just a note

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,7 @@
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
+// compiled file. Don't forget to add bootstrap when needed (e.g. flash message).
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
